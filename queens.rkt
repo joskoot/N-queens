8edab212eba85a3e461520c907bcfc0331b1ba7a
Jos Koot <jos.koot@gmail.com>
2022-03-04 00:26:57 +0100

    Add files via upload

--- queens.rkt
+++ queens.rkt
@@ -3,60 +3,30 @@
 (provide queens)
 (require (only-in 2htdp/image square rectangle star overlay text/font))
 
-; The N-queens puzzle
-;
-; Both files and ranks are identified by the numbers 0 up to but not including N.
-; A chess player identifies files by the letters of the alphabet.
-; Natural numbers are easier when given a file f we want the next file.
-; An encoding in letters is possible, of course, but would slow down the program.
-; Keeping record of all attacked fields when placing a queen on the board
-; does not accelerate the program and requires much more memory.
-; Hence, no such recording is made.
-
 (define (queens N (show-solutions? #t) (whole-classes? #t) (show-boards? #t))
  
- ; A (partial) solution is a list of ranks in reversed order of files.
- ; Hence, the last element of the list is the rank in file 0 (= file a).
- ; Whether the first or the last element of a partial solution corresponds to file 0 is important
- ; in procedure safe? and attacks?. It is a choise, but once made we must stick to it.
- 
- (define (generate-solutions partial-N) ; Place queens in files 0 up to but not including partial-N.
-  (cond                                 ; With partial-N = N we have a full solution.
-   ((zero? partial-N) '(())) ; We want a list of solutions, each solution being a list. Hence (()).
-   (else ; Loop for each partial-solution of partial-N files.
-    (let loop1
-     ((old-partial-solutions (generate-solutions (sub1 partial-N)))
-      (new-partial-solutions '()))
-     (cond
-      ((null? old-partial-solutions) new-partial-solutions)
-      (else
-       (let
-        ((old-partial-solution (car old-partial-solutions))
-         (old-partial-solutions (cdr old-partial-solutions)))
-        ; Nested loop for each rank in file partial-N.
-        (let loop2 ((rank 0) (new-partial-solutions new-partial-solutions))
-         (cond
-          ((>= rank N) (loop1 old-partial-solutions new-partial-solutions))
-          (else
-           (cond
-            ((safe? old-partial-solution rank partial-N) ; Does nested loop over files.
-             (loop2 (add1 rank) (cons (cons rank old-partial-solution) new-partial-solutions)))
-            (else (loop2 (add1 rank) new-partial-solutions)))))))))))))
+ (define (generate-solutions)
+  (let file-loop ((file 0) (occupied-files '()) (old-solutions '(())))
+   (cond
+    ((= file N) old-solutions)
+    ((null? old-solutions) '())
+    (else
+     (let solution-loop ((old-solutions old-solutions) (new-solutions '()))
+      (cond
+       ((null? old-solutions) (file-loop (add1 file) (cons file occupied-files) new-solutions))
+       (else
+        (let ((old-solution (car old-solutions)) (old-solutions (cdr old-solutions)))
+         (let rank-loop ((rank 0) (new-solutions new-solutions))
+          (cond
+           ((= rank N) (solution-loop old-solutions new-solutions))
+           ((for/or ((old-rank (in-list old-solution)) (old-file (in-list occupied-files)))
+             (attacks? old-file old-rank file rank))
+            (rank-loop (add1 rank) new-solutions))
+           (else (rank-loop (add1 rank) (cons (cons rank old-solution) new-solutions)))))))))))))
 
- (define (safe? partial-solution new-rank new-file)
-  (let loop ((old-file (sub1 new-file)) (list-of-ranks partial-solution))
-   ; Start with old-file = new-file = partial-N, because the choise is made
-   ; to interpret a solution as a list of ranks in reversed order of files.
-   (or (null? list-of-ranks)
-    (let ((old-rank (car list-of-ranks)))
-     (and
-      (not (attacks? old-file old-rank new-file new-rank))
-      (loop (sub1 old-file) (cdr list-of-ranks)))))))
- 
  (define (attacks? file-a rank-a file-b rank-b)
   (or
    (= rank-a rank-b)
-   (= file-a file-b)
    (= (+ file-a rank-a) (+ file-b rank-b))
    (= (- file-a rank-a) (- file-b rank-b))))
  
@@ -83,6 +53,14 @@
    ((2) '(E R R2 R3))
    ((4) '(E R2))
    ((8) '(E))
+   (else (error "wrong-c-size"))))
+ 
+ (define (find-generators class-size)
+  (case class-size
+   ((1) '(E))
+   ((2) '(E Sv=Sh=Sd2=Sd2))
+   ((4) '(E R=R3 Sv=Sh Sd1=Sd2))
+   ((8) '(E R R2 R3 Sv Sh Sd1 Sd2))
    (else (error "wrong-c-size"))))
  
  (define (make-symmetry-operations)
@@ -113,19 +91,19 @@
    ((and (= (car solution-1) (car solution-2)) (solution<? (cdr solution-1) (cdr solution-2))))
    (else #f)))
 
- (define all-solutions (generate-solutions N))
+ (define all-solutions (generate-solutions))
  (define classes (make-classes))
  (define nr-of-solutions (length all-solutions))
  (define nr-of-classes (length classes))
  
- (define (print-board lst)
-  (define rank-vector (list->vector lst))
+ (define (print-board solution)
+  (define rank-vector (list->vector solution))
   (define n (vector-length rank-vector))
-  (define cyan-field (square 20 "solid" "cyan"))
+  (define cyan-field   (square 20 "solid" "cyan"))
   (define yellow-field (square 20 "solid" "yellow"))
-  (define white-field (square 20 "solid" "white"))
-  (define space (rectangle 3 20 "solid" "white"))
-  (define queen (star 13 "solid" "black"))
+  (define white-field  (square 20 "solid" "white"))
+  (define space   (rectangle 3 20 "solid" "white"))
+  (define queen          (star 13 "solid" "black"))
   (for ((k (in-range 0 n)))
    (display
     (overlay
@@ -154,7 +132,7 @@
 
  (define (print-results)
   (printf "~n~n")
-  (printf line)
+  (print-line)
   (printf "N : ~a~n" N)
   (printf "Number of solutions : ~a~n" nr-of-solutions)
   (printf "Number of classes : ~a~n" nr-of-classes)
@@ -162,8 +140,11 @@
   (when show-solutions?
    (for ((class (in-list classes)) (n (in-naturals 1)))
     (define nr (length class))
-    (printf "~nClass ~s, symmetries ~s, nr of symmetrically equivalent solutions: ~s~n"
-     n (find-symmetries nr) nr)
+    (printf "~nClass ~s~n~
+             Symmetries: ~s~n~
+             Generators: ~s~n~
+             Nr of symmetrically equivalent solutions: ~s~n"
+     n (find-symmetries nr) (find-generators nr) nr)
     (cond
      (show-boards?
       (newline)
@@ -172,9 +153,10 @@
      (else
       (printf "~s~n" (car class))
       (when whole-classes? (for ((sol (in-list (cdr class)))) (printf "~s~n" sol)))))))
-  (printf line))
+  (print-line))
 
- (define line (string-append (make-string 75 #\―) "~n"))
+ (define line (make-string 75 #\―))
+ (define (print-line) (displayln line))
 
  (define (nr-of-classes-of-given-size)
   (let loop ((class-size '(1 2 4 8)) (done 0))
@@ -189,4 +171,3 @@
  
  (print-results)
  (list N nr-of-solutions nr-of-classes (nr-of-classes-of-given-size)))
-
